07ae4ce6f356d85142ca39ca17567842d3909995
oleksiykalashnyk <68416432+oleksiykalashnyk@users.noreply.github.com>
2020-11-12 22:29:39 +0100

    start

---
+++ assets/scripts/main.js
@@ -0,0 +1,15 @@
+const animationLogin = bodymovin.loadAnimation({
+    container: document.getElementById('svgLogin'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'assets/animations/login.json'
+  });
+
+  const animationLogin1 = bodymovin.loadAnimation({
+    container: document.getElementById('svgLogin1'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'assets/animations/login.json'
+  });
---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const sass = require('gulp-sass');
+const rename = require("gulp-rename");
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+
+
+// Static server
+gulp.task('server', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+
+gulp.task('styles', function () {
+    return gulp.src('sass/*.+(scss|sass)')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(rename({
+            prefix: "",
+            suffix: ".min",
+        }))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("css"))
+        .pipe(browserSync.stream());
+});
+
+
+gulp.task("watch", function () {
+    gulp.watch('sass/*.+(scss|sass)', gulp.parallel("styles"));
+    gulp.watch('*.html').on("change", browserSync.reload);
+});
+
+gulp.task('default', gulp.parallel("watch", "server", "styles"));
